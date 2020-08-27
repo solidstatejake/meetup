a8849170a678b952cd68de0f7e3b7271494cef18
ssj <jberg3@binghamton.edu>
2020-08-26 23:25:25 -0400

    [split] Bring in dummy data from <App/>

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const imgUrl = "https://picsum.photos/400";
+
+export default [
+  {
+    id: "m1",
+    title: "Coding Bootcamp",
+    subtitle: "Learn web development the right way",
+    description:
+      "Learning web development is hard! We've all been there. At this meetup, Laff Y. Taff will show new developers the ropes.",
+    imgUrl,
+  },
+  {
+    id: "m1",
+    title: "Coding Bootcamp",
+    subtitle: "Learn web development the right way",
+    description:
+      "Learning web development is hard! We've all been there. At this meetup, Laff Y. Taff will show new developers the ropes.",
+    imgUrl,
+  },
+  {
+    id: "m1",
+    title: "Coding Bootcamp",
+    subtitle: "Learn web development the right way",
+    description:
+      "Learning web development is hard! We've all been there. At this meetup, Laff Y. Taff will show new developers the ropes.",
+    imgUrl,
+  },
+  {
+    id: "m2",
+    title: "Svelte vs React",
+    subtitle: "Code off!",
+    description:
+      "Svelters and Reactors will go head-to-head in this meeting of the frontend minds! Join us to find out who can build a Yelp replica faster.",
+    imgUrl,
+  },
+];
