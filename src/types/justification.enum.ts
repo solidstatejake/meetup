5dafa8e6dd88d2ce6d28761e724f3a92573f81a7
ssj <jberg3@binghamton.edu>
2020-09-01 19:56:13 -0400

    [mk] Justification interface file

---
+++ src/types/justification.enum.ts
@@ -0,0 +1,17 @@
+/**
+ * Represents the justification of an element along an axis.
+ * 
+ * @param left-
+ *    Alignment element to the left
+ * 
+ * @param center-
+ *    Alignment element to the center
+ * 
+ * @param right-
+ *    Alignment element to the right
+ */
+export enum Justification {
+  left = -1,
+  center = 0,
+  right = 1
+}
