35010e965e1d5c5f8ffdd937c64fdb2e9dd0167c
ssj <jberg3@binghamton.edu>
2020-09-04 16:33:48 -0400

    Document component. Use Sass instead of SCSS for brevity.

--- src/components/UI/MenuIcon.svelte
+++ src/components/UI/MenuIcon.svelte
@@ -1,66 +1,49 @@
-<script>
+<script lang='typescript'>
+  /** Indicates whether the menu containing this icon is open.  */
   export let open = false;
 </script>
 
-<div class="menu" class:open>
-  <div class="bar bar-1" />
-  <div class="bar bar-2" />
-  <div class="bar bar-3" />
+<!-- 
+  @component
+  A white, tribar menu icon.
+  ## Notes
+  - This icon is meant to be used in conjunction with `<Menu/>`
+  - The z-index of this component is defined inside of `<Menu/>`
+  - This icon transforms from the tribar to a red, underlined 'X' when `<Menu/>` is opened.
+  - Sass is used (vs SCSS) in this file since there is little code to worry about formatting (formatting doesn't work in VS Code when Sass is chosen to be the styling language in Svelte components).
+-->
+<div class="menu-container" class:open>
+  <div class="menu-bar menu-bar-1" />
+  <div class="menu-bar menu-bar-2" />
+  <div class="menu-bar menu-bar-3" />
 </div>
 
-<style lang="scss">
-  .bar {
-    background: var(--off-white);
-    max-height: 4.5px;
-    min-height: 4.5px;
-    width: 40px;
-    margin-bottom: 5px;
-    border-radius: 1px;
-    transform-style: preserve-3d;
-    transform-origin: center;
-    transition: {
-      property: background, transform, width;
-      duration: 300ms;
-      timing-function: cubic-bezier(0.37, 0.81, 0.6, 0.9);
-    }
-  }
-
-  .open {
-    .bar {
-      &-1 {
-        transform: translateY(10px) rotateZ(45deg) rotateY(360deg);
-        background: var(--danger);
-      }
-      &-2 {
-        transform: translateY(30px) rotateY(360deg);
-        background: var(--danger);
-      }
-      &-3 {
-        transform: translateY(-10px) rotateZ(-45deg) rotateY(360deg);
-        background: var(--danger);
-      }
-    }
-  }
-
-  .menu {
-    cursor: pointer;
-    perspective: 50px;
-  }
-
-  /* 
-    .bar-1 {
-      animation: wave 500ms ease infinite alternate paused;
-    }
-    .bar-2 {
-      animation: wave 500ms 167ms ease infinite alternate paused;
-    }
-    .bar-3 {
-      animation: wave 500ms 334ms ease infinite alternate paused;
-    }
+<style lang="sass">
+  .menu
+    &-bar
+      background: var(--off-white)
+      border-radius: 1px
+      height: 4.5px
+      margin-bottom: 5px
+      transform-origin: center
+      transform-style: preserve-3d
+      transition:
+        property: background, transform, width
+        duration: 300ms
+        timing-function: cubic-bezier(0.37, 0.81, 0.6, 0.9)
+      width: 40px
     
-  @keyframes wave {
-    100% {
-      transform: scale(1.3);
-    }
-  } */
+    &-container 
+      cursor: pointer
+      perspective: 50px
+  
+  .open 
+    .menu-bar-1 
+      transform: translateY(10px) rotateZ(45deg) rotateY(360deg) scale(0.6)
+    .menu-bar-2 
+      transform: translateY(15px) rotateY(360deg) scale(0.6)
+    .menu-bar-3 
+      transform: translateY(-10px) rotateZ(-45deg) rotateY(360deg) scale(0.6)
+    > * 
+      background: var(--danger)
 </style>
