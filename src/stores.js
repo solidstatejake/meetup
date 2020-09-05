00c9f91d6c9f9e2615d1b18df171fd145a6e5b2f
ssj <jberg3@binghamton.edu>
2020-09-05 03:22:56 -0400

    ✅ Create store for menu open/closed state

---
+++ src/stores.js
@@ -0,0 +1,13 @@
+import { writable } from 'svelte/store';
+
+const createMenuState = () => {
+  const { subscribe, set, update } = writable( false );
+
+  return {
+    subscribe,
+    toggle: () => update( state => !state ),
+    open: () => set( true ),
+    close: () => set( false )
+  };
+};
+export const menu = createMenuState();
