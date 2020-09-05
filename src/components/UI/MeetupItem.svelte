47f6f28b1a7bd77a5e984aabb310326e7ec1301e
ssj <jberg3@binghamton.edu>
2020-09-05 06:21:12 -0400

    ✏️ SCSS => Sass | 🎨

--- src/components/UI/MeetupItem.svelte
+++ src/components/UI/MeetupItem.svelte
@@ -1,7 +1,6 @@
 <script>
   import Button from "./Button.svelte";
   import { createEventDispatcher } from "svelte";
-  import shortid from "shortid";
 
   export let title;
   export let subtitle;
@@ -36,87 +35,95 @@
     </div>
 
     <div class="footer">
-      <Button color="danger" small pill on:click={deleteMeetup}>Delete</Button>
-      <Button color="info" small pill>Edit</Button>
-      <Button color="primary" small pill>Share</Button>
+      <Button
+        color="danger"
+        small
+        circle
+        icon={{ justification: 'center', label: 'btn', name: 'trash', color: '#333333' }}
+        on:click=     {deleteMeetup}>
+        Delete
+      </Button>
+      <Button
+        color="info"
+        small
+        circle
+        icon={{ justification: 'center', label: 'btn', name: 'pencil', color: '#333333' }}>
+        Edit
+      </Button>
+      <Button
+        color="primary"
+        small
+        circle
+        icon={{ justification: 'center', label: 'btn', name: 'share', color: '#333333' }}>
+        Share
+      </Button>
     </div>
 
   </div>
 </article>
 
-<style lang="scss">
-  article {
-    background: white;
-    margin-bottom: 2rem;
-    width: 360px;
-  }
-
-  h1 {
-    font-size: var(--font-size-h1-sm);
-    margin-bottom: 5px;
-  }
-
-  h3 {
-    font-size: var(--font-size-h3-sm);
-    font-weight: lighter;
-  }
-
-  img {
-    max-height: 324px;
-    width: 100%;
-  }
-  p {
-    font-size: var(--font-size-p-sm);
-    font-weight: 400;
-  }
-
-  .content {
-    display: flex;
-    padding: 1rem;
-    padding-bottom: 0;
-    flex-direction: column;
-    height: 18rem;
-    justify-content: space-evenly;
-  }
-
-  .description {
-    margin-top: 1rem;
-    line-height: 1.8rem;
-  }
-
-  .footer {
-    align-items: center;
-    display: flex;
-    height: 100%;
-    justify-content: flex-end;
-    width: 100%;
-    gap: 1rem;
-  }
-
-  .icon {
-    margin-left: auto;
-    margin-top: auto;
-  }
-
-  @media only screen and (min-width: 380px) {
-    article {
-      border-radius: 3px;
-      box-shadow: var(--card-shadow);
-      margin-bottom: 0;
-      transition-property: background-color, box-shadow, color, transform;
-      transition-duration: 300ms;
-      transition-timing-function: cubic-bezier(0.18, 0.89, 0.32, 1.28);
-      transform-style: preserve-3d;
-
-      &:hover {
-        transform: perspective(100px) scale(1.03) translateY(-5px);
-        box-shadow: var(--btn-hover-shadow);
-      }
-    }
-  }
-  @media only screen and (min-width: 880px) {
-    article {
-      width: 400px;
-    }
-  }
+<style type='text/sass'>
+  article 
+    background: var(--off-white)
+    margin-bottom: 2rem
+    transition: background-color 300ms cubic-bezier(0.18, 0.89, 0.32, 1.28), box-shadow 300ms cubic-bezier(0, 0.85, 0.29, 1), color 300ms cubic-bezier(0, 0.85, 0.29, 1), transform 300ms cubic-bezier(0, 0.85, 0.29, 1), width 1000ms cubic-bezier(0, 0.85, 0.29, 1)
+    transform-style: preserve-3d
+    width: 100%
+  
+  h1 
+    font-size: var(--font-size-h1-sm)
+    margin-bottom: 5px
+  
+  h3 
+    font-size: var(--font-size-h3-sm)
+    font-weight: lighter
+  
+  img 
+    max-height: 324px
+    width: 100%
+  
+  p 
+    font-size: var(--font-size-p-sm)
+    font-weight: 400
+  
+  .content 
+    display: flex
+    padding: 1rem
+    padding-bottom: 0
+    flex-direction: column
+    height: 18rem
+    justify-content: space-evenly
+  
+  .description 
+    margin-top: 1rem
+    line-height: 1.8rem
+  
+  .footer 
+    align-items: center
+    display: flex
+    height: 100%
+    justify-content: flex-end
+    width: 100%
+    gap: 1rem
+  
+  .icon 
+    margin-left: auto
+    margin-top: auto
+  
+  @media only screen and (min-width: 400px) 
+    article 
+      border-radius: 3px
+      box-shadow: var(--card-shadow)
+      background-color: white
+      margin-bottom: 0
+      width: 400px
+      &:hover 
+        box-shadow: var(--btn-hover-shadow)
+        transform: perspective(100px) scale(1.03) translateY(-5px)
+      
+  @media only screen and (min-width: 880px) 
+    article 
+      width: 400px
+    
+  
 </style>
