eeeb86b949be20e0e95c1adffb377fc95a557b05
ssj <jberg3@binghamton.edu>
2020-09-05 08:37:35 -0400

    🛠 Create close button

---
+++ src/components/Meetups/Modal.svelte
@@ -0,0 +1,71 @@
+<script>
+  import { fade } from "svelte/transition";
+
+  export let open = false;
+
+  const close = () => (open = false);
+</script>
+
+<div class="background-cover" out:fade={{ duration: 200 }} on:click={close} />
+
+<div class="modal" in:fade={{ duration: 200 }} out:fade={{ duration: 200 }}>
+  <span class="close-icon" on:click={close}>
+    <div class="bar bar-1" />
+    <div class="bar bar-2" />
+  </span>
+  <slot />
+</div>
+
+<style lang="scss">
+  $modal-z-index: 999;
+
+  .background-cover {
+    background-color: rgba(0, 0, 0, 0.15);
+    height: 100%;
+    left: 0;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: $modal-z-index - 1;
+  }
+
+  .bar {
+    background: grey;
+    border-radius: 1px;
+    height: 4.5px;
+    margin-bottom: 5px;
+    margin-left: auto;
+    transform-origin: center;
+    transition: {
+      duration: 300ms;
+      property: background, transform;
+      timing-function: cubic-bezier(0.37, 0.81, 0.6, 0.9);
+    }
+    width: 30px;
+
+    &-1 {
+      transform: rotateZ(45deg);
+    }
+
+    &-2 {
+      transform: translateY(-10px) rotateZ(-45deg);
+    }
+  }
+
+  .close-icon:hover {
+    > * {
+      background-color: var(--danger);
+    }
+  }
+  
+  .modal {
+    background-color: var(--off-white);
+    box-shadow: var(--btn-hover-shadow);
+    left: 50vw;
+    padding: 3rem;
+    position: fixed;
+    top: 50vh;
+    transform: translate(-50%, -50%);
+    z-index: $modal-z-index;
+  }
+</style>
