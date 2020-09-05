f8913642fc53a2615f1af8cd216d21e86a268339
ssj <jberg3@binghamton.edu>
2020-09-05 06:19:25 -0400

    🎨 ✏️

--- src/components/containers/Footer.svelte
+++ src/components/containers/Footer.svelte
@@ -2,43 +2,16 @@
 
 </script>
 
-<footer>
-  <span class='rocket-1'>
-    🚀
-  </span>
-
-  <span class='rocket-2'>
-    🚀
-  </span>
-
-  <span class='rocket-3'>
-    🚀
-  </span>
-</footer>
+<footer> &copy; solidstate::designco </footer>
 
 <style lang='sass'>
   @use 'sass:color'
-  
-
   footer
-    align-items: center
     background-color: scale-color($color: #4b30f5, $lightness: 20% ) // where --indigo: #4b30f5
-    display: flex
-    height: 10rem
-    justify-content: space-around
+    bottom: 0
+    height: 20rem
     margin-top: 10rem
+    position: relative
+    text-align: center
     width: 100%
-
-  .rocket
-    &-1
-      transform: rotate(15deg)
-      font-size: 2rem
-    &-2
-      font-size: 4rem
-      transform: rotate(45deg)
-      margin-bottom: 8rem
-    &-3
-      transform: rotate(60deg)
-      font-size: 2rem
-
 </style>
