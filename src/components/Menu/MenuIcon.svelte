3f66f6b73199368c7b003e83070509899ed64adc
ssj <jberg3@binghamton.edu>
2020-09-07 18:33:50 -0400

    Convert Sass => SCSS

--- src/components/Menu/MenuIcon.svelte
+++ src/components/Menu/MenuIcon.svelte
@@ -17,32 +17,39 @@
   <div class="icon-bar icon-bar-3" />
 </div>
 
-<style lang="sass">
-  .icon
-    &-bar
-      background: var(--off-white)
-      border-radius: 1px
-      height: 4.5px
-      margin-bottom: 5px
-      transform-origin: center
-      transform-style: preserve-3d
-      transition:
-        property: background, transform, width
-        duration: 300ms
-        timing-function: cubic-bezier(0.37, 0.81, 0.6, 0.9)
-      width: 40px
-    
-    &-container 
-      cursor: pointer
-      perspective: 50px
-  
-  .menu-is-open 
-    .icon-bar-1 
-      transform: translateY(10px) rotateZ(45deg) rotateY(360deg) scale(0.6)
-    .icon-bar-2 
-      transform: translateY(15px) rotateY(360deg) scale(0.6)
-    .icon-bar-3 
-      transform: translateY(-10px) rotateZ(-45deg) rotateY(360deg) scale(0.6)
-    > * 
-      background: var(--danger)
+<style lang="scss">
+  .icon {
+    &-bar {
+      background: var(--almost-white);
+      border-radius: 1px;
+      height: 4.5px;
+      margin-bottom: 5px;
+      transform-origin: center;
+      transform-style: preserve-3d;
+      transition: {
+        property: background, transform, width;
+        duration: 300ms;
+        timing-function: cubic-bezier(0.37, 0.81, 0.6, 0.9);
+      }
+      width: 40px;
+    }
+    &-container {
+      cursor: pointer;
+      perspective: 50px;
+    }
+  }
+  .menu-is-open {
+    .icon-bar-1 {
+      transform: translateY(10px) rotateZ(45deg) rotateY(360deg) scale(0.6);
+    }
+    .icon-bar-2 {
+      transform: translateY(15px) rotateY(360deg) scale(0.6);
+    }
+    .icon-bar-3 {
+      transform: translateY(-10px) rotateZ(-45deg) rotateY(360deg) scale(0.6);
+    }
+    > * {
+      background: var(--danger);
+    }
+  }
 </style>
